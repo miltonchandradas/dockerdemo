b3b939f148739140d36402f3f406cbd55237d427
Milton Chandradas <milton.chandradas@sap.com>
2020-10-16 00:49:17 -0400

    initial commit...

--- server.js
+++ server.js
@@ -5,23 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const handleProductsRequest = async (req, res) => {
-    const response = await axios({
-        method: "GET",
-        url: "https://services.odata.org/V2/Northwind/Northwind.svc/Products",
-        params: {
-            $format: "json"
-        },
-        headers: {
-            accept: "application/json"
-        }
-    });
-    res.send(response.data.d.results);
-}
+   const response = await axios.get(
+      "https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json"
+   );
+   res.send(response.data.d.results);
+};
+
+const handleSuppliersRequest = async (req, res) => {
+   const response = await axios.get(
+      "https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json"
+   );
+   res.send(response.data.d.results);
+};
 
 app.get("/products", handleProductsRequest);
+app.get("/suppliers", handleSuppliersRequest);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+   console.log(`Listening on port ${PORT}`);
 });
-
-
